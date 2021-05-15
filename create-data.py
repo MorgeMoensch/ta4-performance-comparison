14caf5529b6de7cea66d0c2e10c079d9abf4fc36
MorgeMoensch <morgemoensch@morgemoensch.dev>
2021-05-15 15:17:01 +0200

    Finialize test of creating dummy json objects and storing them in a file

--- create-data.py
+++ create-data.py
@@ -1,11 +1,26 @@
 import uuid
 import random
 import string
+import json
 
-print(uuid.uuid4())
 
 def random_string_generator():
     return ''.join(random.choice(string.ascii_letters) for x in range(25))
 
+list = []
+
+# Add new DummyObjects to List
 for x in range(20):
-    print(random_string_generator())
+    newObj = {
+            'id': uuid.uuid4().__str__(),
+            'name': random_string_generator()
+            }
+    list.append(newObj)
+
+
+#Serializing JSON
+json_object = json.dumps(list, indent = 4)
+
+#Write to file
+with open("test.json", "w") as outfile:
+    outfile.write(json_object)
