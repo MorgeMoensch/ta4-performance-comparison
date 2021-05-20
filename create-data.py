dea16b4881231ec061e2617c93f6c16ec5a2f480
MorgeMoensch <62468040+MorgeMoensch@users.noreply.github.com>
2021-05-20 19:47:30 +0200

    Update Generator Code to create 2000 Entries that are modelled after the original. (#2)

--- create-data.py
+++ create-data.py
@@ -2,18 +2,30 @@ import uuid
 import random
 import string
 import json
+import datetime
 
-
-def random_string_generator():
-    return ''.join(random.choice(string.ascii_letters) for x in range(25))
+def random_string_generator(length = 25):
+    return ''.join(random.choice(string.ascii_letters) for x in range(length))
 
 list = []
 
 # Add new DummyObjects to List
-for x in range(20):
+for x in range(2000):
     newObj = {
             'id': uuid.uuid4().__str__(),
-            'name': random_string_generator()
+            'a': random_string_generator(),
+            'createdAt': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'bd': {
+                    'tN': random_string_generator(),
+                    's': random_string_generator(10),
+                    'id': uuid.uuid4().__str__(),
+                    'ap': {
+                        'n': random_string_generator(),
+                        'abbr': random_string_generator(6),
+                        'id': uuid.uuid4().__str__()
+                    }
+
+                }
             }
     list.append(newObj)
 
